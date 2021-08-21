6427476c7f474d9f24e760fda8fb2e08bde2be9e
dell-mac <adelle@botanicbaby.com.au>
2021-08-21 16:47:24 +1000

    updated sentence

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Melbourne Lockdown!'));
+app.get('/', (req, res) => res.send('Hello Melbourne Lockdown! It is just another two weeks.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
